1e060d02d738da5fa16ad8c816d51e7bf570fd96
Vladislav <tomasciuc.vladislav@gmail.com>
2025-07-24 10:34:02 +0300

    translate

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -183,7 +183,7 @@ export type LeadsConfig = {
   successMessage?: string;
 };
 
-const defaultWelcomeMessage = 'Hi there! How can I help?';
+const defaultWelcomeMessage = 'Привет! Чем я могу помочь?';
 
 /*const sourceDocuments = [
     {
