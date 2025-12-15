7bf15edf94a455d03e40ec0b8cc83b2bc29fcbe5
Dasha <dasha32645@gmail.com>
2025-12-15 18:43:37 +0300

    small fix

--- src/components/bubbles/StarterPromptBubble.tsx
+++ src/components/bubbles/StarterPromptBubble.tsx
@@ -10,22 +10,18 @@ export const StarterPromptBubble = (props: Props) => (
       type="button"
       data-modal-target="defaultModal"
       data-modal-toggle="defaultModal"
-      class={`flex justify-start items-start animate-fade-in host-container hover:brightness-90 active:brightness-75 ${
+      class={`px-3 py-2.5 ml-1 whitespace-pre-wrap max-w-full rounded-lg rounded-tr-none text-gray-880 animate-fade-in host-container cursor-pointer hover:brightness-90 active:brightness-75 ${
         props.disabled ? 'opacity-60 cursor-default' : ''
       }`}
       data-testid="host-bubble"
       disabled={props.disabled}
       onClick={() => !props.disabled && props.onPromptClick?.()}
+      style={{
+        width: 'max-content',
+        background: 'var(--gradient, linear-gradient(135deg, #B4FF0A 0%, #A4EB04 100%))',
+      }}
     >
-      <span
-        class="px-3 py-2.5 ml-1 whitespace-pre-wrap max-w-full rounded-lg rounded-tr-none text-gray-880"
-        style={{
-          width: 'max-content',
-          background: 'var(--gradient, linear-gradient(135deg, #B4FF0A 0%, #A4EB04 100%))',
-        }}
-      >
-        {props.prompt}
-      </span>
+      {props.prompt}
     </button>
   </>
 );
